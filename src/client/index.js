27be89f89fd589555bf6d8889f9003f7694da395
Ibrahim Althani <althani@Ibrahims-MacBook-Pro.local>
2020-07-26 14:51:07 +0300

    updating files to support funcionality and minify css

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,18 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
-
+import { checkForName } from './js/nameChecker.js'
+import { handleSubmit } from './js/formHandler.js'
+// importing styles to convert in SASS loader.
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
+// exporting to client library
+export {
+    checkForName,
+    handleSubmit
+}
