042f1ec9541476de0fbfde315e07778b6c15f31e
Ibrahim Althani <althani@Ibrahims-MacBook-Pro.local>
2020-07-27 12:31:43 +0300

    Added JS functionality, fixed SASS errors, separated build and production modes successfully. Live server functioning correctly

--- src/client/index.js
+++ src/client/index.js
@@ -6,10 +6,10 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-console.log(checkForName);
+
 
 alert("I EXIST")
-console.log("CHANGE!!");
+console.log("HEY!!!!");
 
 // exporting to client library
 export {
