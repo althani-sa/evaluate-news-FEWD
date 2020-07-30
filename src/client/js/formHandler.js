d72d6bb27bfff21c72dcc69c5f63b192dbb65c5d
Ibrahim Althani <althani@Ibrahims-MacBook-Pro.local>
2020-07-31 02:01:16 +0300

    SUMMARY: Styling pages. Added styles and modified SCSS files to create gradient background and customized hover options on items on the DOM. AYLEIN API successfully calls and retrieves sentiment data. Added __test__ directory to create JEST testing on formHandler.js and nameChecker.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,15 +1,18 @@
+import { json } from "body-parser";
+
 export const handleSubmit = (e) => {
     e.preventDefault()
 
     // check what text was put into the form field
     let URL = document.getElementById('name').value
-
     Client.checkForLink(URL)
 
 
     let parseURL = {
         url: URL
     };
+// This is fine as it is, but it would be nicer to see this separated into another function to make sure that no requests go into the server.
+// unless if they are URLS! I don't want to break it.
     fetch('http://localhost:8081/aylien', {
 
         headers: {
@@ -20,8 +23,10 @@ export const handleSubmit = (e) => {
         body: JSON.stringify(parseURL)
     })
     .then(res => res.json())
-    .then(function(res) {
-            document.getElementById('form__results').innerHTML = res.polarity
+        .then((res) => {
+            document.getElementById('form__results').innerHTML = `The link's overall sentiment is... ${res.polarity}`
+            document.getElementById('confidence__results').innerHTML = `Confidence in result is... ${res.polarity_confidence * 100}%`
+            // test to see result in console console.log(res);
     })
 }
 
