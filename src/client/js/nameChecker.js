35fdcf8641d0fbf8ff75cc14114e224c3955b3be
Ibrahim <ibrahim@ibrahim.ai>
2021-07-23 03:48:13 +0300

    updated condition

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,10 +1,11 @@
-export const checkForLink = (inputText = '') =>{
-    let linkChecker = RegExp(/^((ftp|http|https):\/\/)?www\.([A-z]+)\.([A-z]{2,})/)
+export const checkForLink = (inputText = '') => {
+	let linkChecker = RegExp(
+		/^((ftp|http|https):\/\/)?www\.([A-z]+)\.([A-z]{2,})/
+	)
 
-    if (linkChecker.test(inputText) != true) {
-        alert("Please enter a valid URL");
-    }
-    else {
-        return linkChecker.test(inputText)
-    }
+	if (!linkChecker.test(inputText)) {
+		alert('Please enter a valid URL')
+	} else {
+		return linkChecker.test(inputText)
+	}
 }
